8597a220d17d28f525df356ba5aa68b4d0468e1a
Diego Varussa <diegovarussa@gmail.com>
2021-09-14 11:08:03 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import WebSocket from 'ws';
 const ws = new WebSocket('wss://ftx.com/ws/');
 
 ws.on('open', function open() {
-  ws.send(JSON.stringify({'op': 'subscribe', 'channel': 'ticker', 'market': 'BTC-PERP'}));
+  ws.send(JSON.stringify({'op': 'subscribe', 'channel': 'ticker', 'market': 'SOL-PERP'}));
 });
 
 ws.on('message', function incoming(message) {
